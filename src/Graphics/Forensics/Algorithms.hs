91e10af5a36d92b8555c9033e69735b566f9d828
Moritz Roth <mr2211@doc.ic.ac.uk>
2011-12-20 02:19:44 +0000

    Fixed default-extensions and type synonyms

--- src/Graphics/Forensics/Algorithms.hs
+++ src/Graphics/Forensics/Algorithms.hs
@@ -7,14 +7,14 @@ import qualified Data.Array.Repa as Repa
 class (Repa.Shape s, Ix i) => RepaArray s i where
   toArray :: Repa.Elt a => Repa.Array s a -> Array i a
 
-instance RepaArray DIM1 Int where
+instance RepaArray (Z :. Int) Int where
   toArray a@ (Repa.Array (Z :. x) _) =
     listArray (1, x) (Repa.toList a)
 
-instance RepaArray DIM2 (Int, Int) where
+instance RepaArray (Z :. Int :. Int) (Int, Int) where
   toArray a@ (Repa.Array (Z :. x :. y) _) =
     listArray ((1, 1), (x, y)) (Repa.toList a)
 
-instance RepaArray DIM3 (Int, Int, Int) where
+instance RepaArray (Z :. Int :. Int :. Int) (Int, Int, Int) where
   toArray a@ (Repa.Array (Z :. x :. y :. z) _) =
     listArray ((1, 1, 1), (x, y, z)) (Repa.toList a)
