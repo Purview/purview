cae90228758c6f2df443198d72be15da04834e8f
Moritz Roth <mr2211@doc.ic.ac.uk>
2011-12-30 01:48:30 +0000

    Add luminance gradient analyser

---
+++ src/Graphics/Forensics/Analyser/LuminanceGradient.hs
@@ -0,0 +1,51 @@
+module Graphics.Forensics.Analyser.LuminanceGradient where
+
+import Graphics.Forensics.Analyser
+import Graphics.Forensics.Algorithms
+import Graphics.Forensics.Color
+import Graphics.Forensics.Image
+import Graphics.Forensics.Report
+import Data.Array.Repa (Z(..), DIM2, (:.)(..))
+import qualified Data.Array.Repa as Repa
+
+sobelX :: [Float]
+sobelX = [-1, 0, 1, -2, 0, 2, -1, 0, 1]
+
+sobelY :: [Float]
+sobelY = [1, 2, 1, 0, 0, 0, -1, -2, -1]
+
+analyser :: Analyser ByteImage
+analyser =
+  Analyser
+  { analyse = lgAnalyse
+  , name = "lg"
+  , author = "David Flemström & Moritz Roth"
+  , version = readVersion "0.1"
+  }
+
+luminanceGradient :: ByteImage -> ByteImage
+luminanceGradient = floatToByteImage .
+                    fragmentMap gradient .
+                    fragmentize Clamp (Z :. 3 :. 3) .
+                    Repa.map rgbaToGrayscale .
+                    byteToFloatImage
+
+rgbaToGrayscale :: RGBA Float -> Float
+rgbaToGrayscale (RGBA r g b _) =
+  0.2126 * r + 0.7152 * g + 0.0722 * b
+
+gradient ::  Repa.Array DIM2 Float -> RGBA Float
+gradient array =
+  RGBA r g b 1.0
+  where
+    lx    = sum $ zipWith (*) (Repa.toList array) sobelX
+    ly    = sum $ zipWith (*) (Repa.toList array) sobelY
+    angle = atan2 ly lx
+    r     = (- sin angle) / 2.0 + 0.5
+    g     = (- cos angle) / 2.0 + 0.5
+    b     = sqrt (lx * lx + ly * ly)
+
+lgAnalyse :: ByteImage -> Analysis ()
+lgAnalyse = reportInfo "Luminance gradient mapped image" .
+            reportImage .
+            luminanceGradient
--- src/Graphics/Forensics/Analysers.hs
+++ src/Graphics/Forensics/Analysers.hs
@@ -2,10 +2,11 @@ module Graphics.Forensics.Analysers where
 
 import Graphics.Forensics.Analyser
 import Graphics.Forensics.Image
-
+import Graphics.Forensics.Analyser.LuminanceGradient as LG
 import Graphics.Forensics.Analyser.Test as Test
 
 analysers :: [Analyser ByteImage]
 analysers =
-  [ Test.analyser
+  [ Test.analyser,
+    LG.analyser
   ]
