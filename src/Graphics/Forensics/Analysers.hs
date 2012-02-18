68cd1b8e66ae9e035aeb945c18bee73fb7880083
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-02-18 18:03:58 +0100

    Add localcfa to analysers

--- src/Graphics/Forensics/Analysers.hs
+++ src/Graphics/Forensics/Analysers.hs
@@ -4,9 +4,10 @@ import Graphics.Forensics.Analyser
 import Graphics.Forensics.Image
 import Graphics.Forensics.Analyser.LuminanceGradient as LG
 import Graphics.Forensics.Analyser.Test as Test
-
+import Graphics.Forensics.Analyser.LocalCFA as LCFA
 analysers :: [Analyser ByteImage]
 analysers =
   [ Test.analyser,
-    LG.analyser
+    LG.analyser,
+    LCFA.analyser
   ]
