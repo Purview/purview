d2dcd80810dc17fb0277257a01d469ba5e3939e7
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-07-26 17:31:24 +0100

    Update luminance gradient to use convolveOutP, work with newest Repa version

--- src/Graphics/Forensics/Analyser/LuminanceGradient.hs
+++ src/Graphics/Forensics/Analyser/LuminanceGradient.hs
@@ -1,22 +1,24 @@
 module Graphics.Forensics.Analyser.LuminanceGradient where
-
+import Graphics.Forensics.Algorithms
 import Graphics.Forensics.Analyser
 import Graphics.Forensics.Color
 import Graphics.Forensics.Image
 import Graphics.Forensics.Report
-import Data.Array.Repa (DIM2, (:.)(..))
+import Data.Array.Repa (Array(..), Z(..), DIM2, (:.)(..), Source(..), D)
 import qualified Data.Array.Repa as Repa
-import Data.Array.Repa.Stencil
+import Data.Array.Repa.Repr.Unboxed (U)
 
-sobelX :: Stencil DIM2 Float
-sobelX = [stencil2| -1 0 1
-                    -2 0 2
-                    -1 0 1 |]
+sobelX :: Array U DIM2 Float
+--sobelX = [stencil2| -1 0 1
+--                    -2 0 2
+--                    -1 0 1 |]
+sobelX = Repa.fromListUnboxed (Z :. 3 :. 3) [-1, 0, 1, -2, 0, 2, -1, 0, 1]
 
-sobelY :: Stencil DIM2 Float
-sobelY = [stencil2| -1 -2 -1
-                     0  0  0
-                     1  2  1 |]
+sobelY :: Array U DIM2 Float
+--sobelY = [stencil2| -1 -2 -1
+--                     0  0  0
+--                     1  2  1 |]
+sobelY = Repa.fromListUnboxed (Z :. 3 :. 3) [-1, -2, -1, 0, 0, 0, 1, 2, 1]
 
 analyser :: Analyser ByteImage
 analyser =
@@ -27,25 +29,24 @@ analyser =
   , version = readVersion "1.0"
   }
 
-luminanceGradient :: ByteImage -> ByteImage
-luminanceGradient img =
-  lgImage `Repa.deepSeqArray` floatToByteImage $ lgImage
-  where
-    grayscale = grayscaleImage . byteToFloatImage $ img
-    (gX, gY)  = grayscale `Repa.deepSeqArray` gradients $ grayscale
-    lgImage   = Repa.force2 $ Repa.zipWith gradientColour gX gY
+luminanceGradient :: (Monad m) => ByteImage -> m (ByteImage)
+luminanceGradient img = do
+  grayscale <- Repa.computeP $ grayscaleImage . byteToFloatImage $ img
+  (gX, gY) <- gradients grayscale
+  lgImage <- Repa.computeP $ Repa.zipWith gradientColour gX gY
+  return (floatToByteImage lgImage)
 
-gradients :: Repa.Array DIM2 Float ->
-             (Repa.Array DIM2 Float, Repa.Array DIM2 Float)
-gradients =
-  Repa.withManifest $ \i ->
-  let (gX, gY) = (edgeX i, edgeY i) in Repa.deepSeqArrays [gX, gY] (gX, gY)
+gradients :: (Monad m) => Array U DIM2 Float ->
+             m ((Array U DIM2 Float, Array U DIM2 Float))
+gradients i = do
+  gX <- edgeX i
+  gY <- edgeY i
+  return (gX, gY)
 
 {-# NOINLINE grayscaleImage #-}
-grayscaleImage :: FloatImage -> Repa.Array DIM2 Float
+grayscaleImage :: FloatImage -> Array D DIM2 Float
 grayscaleImage =
-  Repa.withManifest $ \i ->
-  Repa.force2 $ Repa.map rgbaToGrayscale i
+  Repa.map rgbaToGrayscale
 
 {-# INLINE rgbaToGrayscale #-}
 rgbaToGrayscale :: RGBA Float -> Float
@@ -53,14 +54,14 @@ rgbaToGrayscale (RGBA r g b _) =
   0.2126 * r + 0.7152 * g + 0.0722 * b
 
 {-# NOINLINE edgeX #-}
-edgeX :: Repa.Array DIM2 Float -> Repa.Array DIM2 Float
-edgeX img =
-  Repa.deepSeqArray img $ Repa.force2 $ mapStencil2 BoundClamp sobelX img
+edgeX :: (Monad m) => Array U DIM2 Float -> m (Array U DIM2 Float)
+edgeX =
+  convolve Clamp sobelX
 
 {-# NOINLINE edgeY #-}
-edgeY :: Repa.Array DIM2 Float -> Repa.Array DIM2 Float
-edgeY img =
-  Repa.deepSeqArray img $ Repa.force2 $ mapStencil2 BoundClamp sobelY img
+edgeY :: (Monad m) => Array U DIM2 Float -> m (Array U DIM2 Float)
+edgeY =
+  convolve Clamp sobelY
 
 {-# INLINE gradientColour #-}
 gradientColour ::  Float -> Float -> RGBA Float
@@ -70,15 +71,15 @@ gradientColour gx gy =
     angle = atan2 gy gx
     r     = getR angle
     g     = getG angle
-    b     = modulus gx gy
-    {-# INLINE modulus #-}
-    modulus x y = sqrt (x * x + y * y)
+    b     = magnitude gx gy
+    {-# INLINE magnitude #-}
+    magnitude x y = sqrt (x * x + y * y)
     {-# INLINE getR #-}
     getR = (0.5 +) . (/ 2) . negate . sin
     {-# INLINE getG #-}
     getG = (0.5 +) . (/ 2) . negate . cos
 
 lgAnalyse :: ByteImage -> Analysis ()
-lgAnalyse = reportInfo "Luminance gradient mapped image" .
-            reportImage .
-            luminanceGradient
+lgAnalyse img = do
+  result <- luminanceGradient img
+  reportInfo "Luminance gradient mapped image" $ reportImage result
