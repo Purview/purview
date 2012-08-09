66bc0db91af64a89e9efd758d2b3891c500acc43
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-08-09 03:10:54 +0100

    Add repa mapM

--- src/Graphics/Forensics/Utilities.hs
+++ src/Graphics/Forensics/Utilities.hs
@@ -1,9 +1,10 @@
 module Graphics.Forensics.Utilities
        (
          zipWith3
+       , mapM
        ) where
 
-import Prelude hiding (zipWith3)
+import Prelude hiding (zipWith3, mapM)
 
 import Data.Array.Repa (Array(..), D, Source(..), Shape(..))
 import qualified Data.Array.Repa as Repa
@@ -16,3 +17,12 @@ zipWith3 :: (Repa.Elt a, Repa.Elt b, Repa.Elt c, Repa.Elt d,
             Array r1 sh a -> Array r2 sh b ->
             Array r3 sh c -> Array D sh d
 zipWith3 = ((Repa.zipWith id .) .) . Repa.zipWith
+
+mapM :: (Repa.Elt a, Repa.Elt b, Source r1 a, Shape sh, Monad m,
+         Repa.Target D b) =>
+        (a -> m b) -> Array r1 sh a -> m (Array D sh b)
+mapM f a = do
+  let mapped = Repa.map f a
+  let sh = extent a
+  result <- sequence . Repa.toList $ mapped
+  return $ Repa.fromList sh result
