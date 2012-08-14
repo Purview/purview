c03dbc3ba68e5b67321a79c14924c061dcc40e66
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-08-14 03:25:29 +0100

    Make fragmentMap use the Repa clamp function for edge pixels instead of cropping them

--- src/Graphics/Forensics/Algorithms.hs
+++ src/Graphics/Forensics/Algorithms.hs
@@ -35,7 +35,7 @@ import Prelude hiding (lookup)
 import qualified Data.Array.Repa.Algorithms.Convolve as Repa
 
 import Data.Array.Repa (Array(..), Z(..), DIM2, (:.)(..),
-                        Source(..), D, Shape(..), (!))
+                        Source(..), D, Shape(..), (!), computeS)
 import Data.Array.Repa.Repr.Unboxed (U, Unbox)
 import qualified Data.Array.Repa as Repa
 import qualified Data.Array.Repa.Eval as Repa
@@ -48,13 +48,16 @@ import Graphics.Forensics.Algorithms.DFT
 -- | Provides the different edge case handling methods for convolution
 data OutOfRangeMode a = Clamp | Value a | Function (Repa.GetOut a)
 
+outOfRangeModeFunction :: OutOfRangeMode a -> Repa.GetOut a
+outOfRangeModeFunction Clamp = Repa.outClamp
+outOfRangeModeFunction (Value a) = Repa.outAs a
+outOfRangeModeFunction (Function f) = f
+
 -- | Convolves a matrix using the specified kernel and edge handling method
 convolve :: (Unbox n, Num n, Monad m) =>
             OutOfRangeMode n -> Array U DIM2 n ->
             Array U DIM2 n -> m (Array U DIM2 n)
-convolve (Clamp)      = Repa.convolveOutP Repa.outClamp
-convolve (Value a)    = Repa.convolveOutP $ Repa.outAs a
-convolve (Function f) = Repa.convolveOutP f
+convolve mode = Repa.convolveOutP (outOfRangeModeFunction mode)
 
 -- | Convolves a matrix using Repa's built in stencil functions
 convolveS :: (Source r1 a) => OutOfRangeMode a ->
@@ -64,20 +67,32 @@ convolveS (Value a)    = mapStencil2 (BoundConst a)
 convolveS (Function _) = undefined
 
 -- | Splits the image into NxM overlapping fragments, then maps a
---   function over each fragment to return a new 2D array
-fragmentMap :: (Repa.Elt a, Num a, Repa.Elt b, Unbox a, Unbox b,
-                Source r1 a) =>
+--   function over each fragment to return a new 2D array.
+fragmentMap :: (Repa.Elt a, Num a, Repa.Elt b, Unbox a, Unbox b) =>
                ((Repa.Array D DIM2 a) -> b) -> DIM2 ->
-               Repa.Array r1 DIM2 a -> Repa.Array D DIM2 b
+               Repa.Array U DIM2 a -> Repa.Array D DIM2 b
 fragmentMap f fragmentSize array =
-  Repa.traverse array newSh traverseFunc
+  Repa.traverse extended newSh traverseFunc
   where
+    extended = extendArray fragmentSize array
     (Z :. sx :. sy) =
       fragmentSize
     newSh (Z :. x :. y) =
       (Z :. x - sx :. y - sy)
     traverseFunc _ ix =
-      f $ getArrayFragment fragmentSize ix array
+      f $ getArrayFragment fragmentSize ix extended
+
+extendArray :: (Unbox e) => DIM2 -> Repa.Array U DIM2 e -> Repa.Array U DIM2 e
+extendArray fragmentSize array =
+  computeS $ Repa.traverse array newSh fillArrayElems
+  where
+    imgSize = extent array
+    (Z :. fragX :. fragY) = fragmentSize
+    xOffset = fragX `div` 2
+    yOffset = fragY `div` 2
+    newSh (Z :. iw :. ih) = (Z :. iw + fragX :. ih + fragY)
+    fillArrayElems get (Z :. ix :. iy) =
+      Repa.outClamp get imgSize (Z :. ix - xOffset :. iy - yOffset)
 
 getArrayFragment :: (Source r1 e) => DIM2 -> DIM2 ->
                     Repa.Array r1 DIM2 e -> Repa.Array D DIM2 e
