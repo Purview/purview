efb3c82684a10a71b3b423ce4179d4394cd74285
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-08-14 03:28:48 +0100

    Fix LocalCFA bugs, now working (still slow):  - Rework diagonal retrieval function  - Use non-parallel DFT so localAnalysis can be evaluated in parallel  - Remove unsafe code

--- src/Graphics/Forensics/Analyser/LocalCFA.hs
+++ src/Graphics/Forensics/Analyser/LocalCFA.hs
@@ -8,11 +8,10 @@ import Graphics.Forensics.Color
 import Graphics.Forensics.Image
 import Graphics.Forensics.Report
 import Data.Array.Repa (Source(..), Z(..),  DIM2, (:.)(..),
-                        U, D, computeP, computeUnboxedS, computeUnboxedP)
+                        U, D, computeP, computeUnboxedP, (!))
 import qualified Data.Array.Repa as Repa
 import qualified Data.Array.Repa.Eval as Repa
 import Data.Array.Repa.Algorithms.Complex
-import System.IO.Unsafe
 
 analyser :: Analyser ByteImage
 analyser =
@@ -27,7 +26,7 @@ localcfa :: (Monad m) => ByteImage -> m ByteImage
 localcfa img = do
   hpf <- highpassFilter . byteToFloatImage $ img
   let fragments = fragmentMap localAnalysis (Z :. 32 :. 32) hpf
-  let filtered = computeUnboxedS $ fragments
+  filtered <- computeUnboxedP $ fragments
   rgbaResult <- computeUnboxedP $ Repa.map fromGrayscaleFloat filtered
   return (floatToByteImage rgbaResult)
 
@@ -39,14 +38,14 @@ highpass = [stencil2| 0  1  0
 highpassFilter :: (Monad m) => FloatImage -> m (Repa.Array U DIM2 Float)
 highpassFilter i = do
   let conv = return . convolveS Clamp highpass
-  g <- getG i >>= conv
-  computeP g
+  computeP =<< conv =<< getG i
 
 localAnalysis :: Repa.Array D DIM2 Float -> Float
-localAnalysis a = unsafePerformIO $ do
-  let diags = map variance $ getDiagonals 32 $ Repa.toList a
-  magnitudes <- dftMagnitude diags
-  return $ getPeakValue . normalise $ magnitudes
+localAnalysis a =
+  getPeakValue . normalise $ magnitudes
+  where
+    diags = getDiagonalVariances a
+    magnitudes = dftMagnitude diags
 
 getG :: (Monad m) => FloatImage -> m (Array U DIM2 Float)
 getG i = computeP $ Repa.map (\(RGBA _ g _ _) -> g * 255) i
@@ -60,28 +59,30 @@ floatMagnitude (r, c) =
     fc = double2Float c
 
 {-# INLINE dftMagnitude #-}
-dftMagnitude :: (Monad m) => [Float] -> m ([Float])
-dftMagnitude a = do
-  let list = Repa.fromList (Z :. (length a)) . map realToComplex $ a
-  dftc <- dftP list
-  return $ map floatMagnitude . Repa.toList $ dftc
-
-getDiagonals :: Int -> [Float] -> [[Float]]
-getDiagonals fragmentSize array = do
-  size <- [0..fragmentSize]
-  return $
-    take (fragmentSize - 2 * size) .
-    getDiagonalValues fragmentSize $
-    (drop size array)
-
---PRE: Array is quadratic
-getDiagonalValues :: Int -> [Float] -> [Float]
-getDiagonalValues fragmentSize array =
-  every (fragmentSize + 1) array
+dftMagnitude :: [Float] -> [Float]
+dftMagnitude a =
+  map floatMagnitude . Repa.toList $ dftc
   where
-    every :: Int -> [a] -> [a]
-    every _ []       = []
-    every n (x : xs) = x : every n (drop n xs)
+    list = Repa.fromList (Z :. (length a)) . map realToComplex $ a
+    dftc = dftS list
+
+-- | Returns the variances of all diagonals in the given array as a list
+getDiagonalVariances :: (Source r1 Float) =>
+                       Repa.Array r1 DIM2 Float -> [Float]
+getDiagonalVariances arr =
+  map variance $ getDiagonals (w + h - 1)
+  where
+    (Z :. w :. h) = extent arr
+    getDiagonals :: Int -> [[Float]]
+    getDiagonals 0 = []
+    getDiagonals s = (getDiagonalAt s 0) : (getDiagonals $ s - 1)
+
+    getDiagonalAt :: Int -> Int -> [Float]
+    getDiagonalAt x n
+      | n >= h || x - n < 0 = []
+      | x - n > w = getDiagonalAt x $ n + 1
+      | otherwise = arr ! (Z :. x - n :. n) : getDiagonalAt x (n + 1)
+
 
 variance :: [Float] -> Float
 variance a =
@@ -99,7 +100,8 @@ getPeakValue :: [Float] -> Float
 getPeakValue list =
   peak / maxVal
   where
-    peak   = maximum . take 3 . drop (mid - 1) $ list
+    peak = maximum . take 3 . drop (mid - 1) $ list
+
     mid    = length list `div` 2
     maxVal = maximum list
 
