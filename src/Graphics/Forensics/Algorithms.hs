b0e0ac3373e916059dba625c5b2581b9339f9524
Moritz Roth <mr2211@doc.ic.ac.uk>
2012-08-25 01:26:13 +0100

    Move convolution to a separate module

--- src/Graphics/Forensics/Algorithms.hs
+++ src/Graphics/Forensics/Algorithms.hs
@@ -32,10 +32,7 @@ module Graphics.Forensics.Algorithms
 
 import Prelude hiding (lookup)
 
-import qualified Data.Array.Repa.Algorithms.Convolve as Repa
-
-import Data.Array.Repa (Array(..), DIM2, Source(..), D, Shape(..))
-import Data.Array.Repa.Repr.Unboxed (U, Unbox)
+import Data.Array.Repa (Source(..), D, Shape(..))
 import qualified Data.Array.Repa as Repa
 
 import Data.Array.Repa.Stencil
@@ -43,27 +40,7 @@ import Data.Array.Repa.Stencil.Dim2
 
 import Graphics.Forensics.Algorithms.DFT
 import Graphics.Forensics.Algorithms.Fragmentize
-
--- | Provides the different edge case handling methods for convolution
-data OutOfRangeMode a = Clamp | Value a | Function (Repa.GetOut a)
-
-outOfRangeFunction :: OutOfRangeMode a -> Repa.GetOut a
-outOfRangeFunction Clamp = Repa.outClamp
-outOfRangeFunction (Value a) = Repa.outAs a
-outOfRangeFunction (Function f) = f
-
--- | Convolves a matrix using the specified kernel and edge handling method
-convolve :: (Unbox n, Num n, Monad m) =>
-            OutOfRangeMode n -> Array U DIM2 n ->
-            Array U DIM2 n -> m (Array U DIM2 n)
-convolve mode = Repa.convolveOutP $ outOfRangeFunction mode
-
--- | Convolves a matrix using Repa's built in stencil functions
-convolveS :: (Source r1 a) => OutOfRangeMode a ->
-             Stencil DIM2 a -> Array r1 DIM2 a -> Array PC5 DIM2 a
-convolveS (Clamp)      = mapStencil2 BoundClamp
-convolveS (Value a)    = mapStencil2 (BoundConst a)
-convolveS (Function _) = undefined
+import Graphics.Forensics.Algorithms.Convolve
 
 scaleArray :: (Source r1 e, Shape sh, Real e) =>
               e -> e -> e -> e ->
